d76c2a2e06972af1ec9592704226fddbe70a59d1
DivianPermana <divian.ajie@gmail.com>
2021-02-08 11:05:29 +0700

    Update resnet.py

--- keras_frcnn/resnet.py
+++ keras_frcnn/resnet.py
@@ -18,10 +18,7 @@ from keras_frcnn.FixedBatchNormalization import FixedBatchNormalization
 
 
 def get_weight_path():
-    if K.image_dim_ordering() == 'th':
-        return 'resnet50_weights_th_dim_ordering_th_kernels_notop.h5'
-    else:
-        return 'resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+    return os.path.join('keras_frcnn', 'weights','resnet50_weights_th_dim_ordering_th_kernels_notop.h5')
 
 
 def get_img_output_length(width, height):
